041c6a142b24eb0322c3079039a5a43011732310
tflanders <tflander@wowway.com>
2013-07-19 10:12:18 -0400

    First pass at creating 2nd part of my ideal DSL

--- src/main/scala/july2013/PollSampler.scala
+++ src/main/scala/july2013/PollSampler.scala
@@ -1,17 +1,18 @@
 package july2013
 
-  case class RawPollSampleWrapper(raw: Seq[PollSample]) {
-    import PollField._
-    def makeTree(field1: PollField.Value, field2: PollField.Value, field3: PollField.Value) = {
-      require(field1 != field2)
-      require(field1 != field3)
-      require(field2 != field3)
+case class RawPollSampleWrapper(raw: Seq[PollSample]) {
+  import PollField._
+  def makeTree(field1: PollField.Value, field2: PollField.Value, field3: PollField.Value): PollTreeWrapper1 = {
+    require(field1 != field2)
+    require(field1 != field3)
+    require(field2 != field3)
+    PollTreeWrapper1(
       raw.groupBy(fieldValForSample(field1, _))
         .mapValues(_.groupBy(fieldValForSample(field2, _))
-          .mapValues(_.groupBy(fieldValForSample(field3, _))))
-    }
+          .mapValues(_.groupBy(fieldValForSample(field3, _)))))
   }
+}
 
-  object PollSampler {
-	  implicit def RawPollDataToRawPollSampleWrapper(raw: Seq[PollSample]) = RawPollSampleWrapper(raw)
-  }
+object PollSampler {
+  implicit def RawPollDataToRawPollSampleWrapper(raw: Seq[PollSample]) = RawPollSampleWrapper(raw)
+}
--- src/test/scala/july2013/PollSamplerTest.scala
+++ src/test/scala/july2013/PollSamplerTest.scala
@@ -3,6 +3,8 @@ package july2013
 import org.scalatest._
 import PollSampler._
 import PollField._
+import PollFieldValue._
+import PollSearcher._
 
 class PollSamplerTest extends FunSpec with ShouldMatchers {
 
@@ -26,6 +28,55 @@ class PollSamplerTest extends FunSpec with ShouldMatchers {
     uglyValidationOne(pollTree)
   }
 
+  it("can tally samples") {
+    val pollTree = rawData.makeTree(Party, Sex, Position)
+
+    pollTree.tally(Democrat)./(Male)./(For) should be(1)
+    pollTree.tally(Democrat)./(Female)./(For) should be(2)
+    pollTree.tally(Democrat)./(Female)./(Against) should be(0)
+  }
+
+  it("can use an implicit wrapper monkey patched against a poll tree") {
+    val pollTree = rawData.makeTree(Party, Sex, Position)
+
+    pollTree.tally(Democrat)./(Male)./(For) should be(1)
+    pollTree.tally(Democrat)./(Female)./(For) should be(2)
+    pollTree.tally(Democrat)./(Female)./(Against) should be(0)
+  }
+
+  it("can build a two-level XPath-like search expression") {
+    Democrat / Male should be(List(Democrat, Male))
+  }
+
+  it("can build a three-level search expression explicitly") {
+    val a: List[PollFieldValue.Val] = Democrat / Male
+    val b: PollFieldValues = PollFieldValues(List(Democrat, Male))
+    val c = b./(For)
+    c should be(PollFieldValues(List(Democrat, Male, For)))
+  }
+  
+  it("can build a three-level XPath-like search expression") {
+    Democrat/Male/For should be(PollFieldValues(List(Democrat, Male, For)))
+  }
+
+    it("can use short syntax for slash to get XPath-like searching") {
+      val pollTree = rawData.makeTree(Party, Sex, Position)
+  
+//      pollTree.tally (Democrat / Female / For) should be (2)  // fails
+//      pollTree tally (Democrat / Female / For) should be (2)  // fails
+      
+      
+      
+      
+//      pollTree tally (Democrat / Female / Against) should be (0)
+      
+  //    pollTree tally Democrat / Male / For should be(1)
+//      pollTree.tally(Democrat)./(Female)./(For) should be(2)
+//      pollTree.tally(Democrat)./(Female)./(Against) should be(0)
+    }
+    
+    // TODO: test requirement for searching all three levels (boo)
+
   it("Can make a tree with fields in any order") {
     val pollTree = rawData.makeTree(Sex, Position, Party)
     uglyValidationTwo(pollTree)
@@ -43,9 +94,9 @@ class PollSamplerTest extends FunSpec with ShouldMatchers {
     }
   }
 
-  def uglyValidationTwo(pollTree: Map[String, Map[String, Map[String, Seq[PollSample]]]]) = {
-    val men = pollTree.get("Male").get
-    val women = pollTree.get("Female").get
+  def uglyValidationTwo(ptr: PollTreeWrapper1) = {
+    val men = ptr.pollTree.get("Male").get
+    val women = ptr.pollTree.get("Female").get
     men.get("For").get.get("Democrat").get.size should be(1)
     men.get("Against").get.get("Democrat").get.size should be(1)
     men.get("For").get.get("Republican") should be(None)
@@ -56,9 +107,9 @@ class PollSamplerTest extends FunSpec with ShouldMatchers {
     women.get("Against").get.get("Republican").get.size should be(1)
   }
 
-  def uglyValidationOne(pollTree: Map[String, Map[String, Map[String, Seq[PollSample]]]]) = {
-    val democrats = pollTree.get("Democrat").get
-    val republicans = pollTree.get("Republican").get
+  def uglyValidationOne(ptr: PollTreeWrapper1) = {
+    val democrats = ptr.pollTree.get("Democrat").get
+    val republicans = ptr.pollTree.get("Republican").get
     democrats.get("Male").get.get("For").get.size should be(1)
     democrats.get("Male").get.get("Against").get.size should be(1)
     democrats.get("Female").get.get("For").get.size should be(2)
