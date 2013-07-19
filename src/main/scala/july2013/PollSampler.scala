9ca9a7d2a2338daa575409c6f48fa323314f0cb5
tflanders <tflander@wowway.com>
2013-07-18 21:56:56 -0400

    created first part of my ideal DSL -- makeTree(...)

--- src/main/scala/july2013/PollSampler.scala
+++ src/main/scala/july2013/PollSampler.scala
@@ -1,18 +1,5 @@
 package july2013
 
-//object PollField extends Enumeration {
-//  type PollField = Value
-//  val Party, Sex, Position = Value
-//
-//  def fieldValForSample(field: Value, sample: PollSample): String = {
-//    field match {
-//      case Party => sample.party
-//      case Sex => sample.sex
-//      case Position => sample.position
-//    }
-//  }
-//}
-
   case class RawPollSampleWrapper(raw: Seq[PollSample]) {
     import PollField._
     def makeTree(field1: PollField.Value, field2: PollField.Value, field3: PollField.Value) = {
